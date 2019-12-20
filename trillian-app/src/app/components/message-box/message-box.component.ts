571789ec2f36dd7b8a9a4d1a2793bd20cae5146b
mregger <eduardoegger@gmail.com>
2019-12-19 21:41:07 -0500

    refactor(app.comp) add message-box.comp and import that instead

---
+++ trillian-app/src/app/components/message-box/message-box.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { IMessage } from '../../interfaces/message.type';
+
+@Component({
+  selector: 'app-message-box',
+  templateUrl: './message-box.component.html',
+  styleUrls: ['./message-box.component.scss']
+})
+export class MessageBoxComponent implements OnInit {
+
+  @Input() message: IMessage;
+
+  @Input() username: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
